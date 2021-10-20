4541b6e83116c2ddfddbd01238f9fd8deec713bc
SzymonKolodziejczyk <szymonnkolodziejczyk@gmail.com>
2021-10-20 02:53:50 +0200

    Spawn Fixed
    Now it spawns the correct amount.

--- Assets/Scripts/Spawn.cs
+++ Assets/Scripts/Spawn.cs
@@ -4,28 +4,32 @@ using UnityEngine;
 
 public class Spawn : MonoBehaviour
 {
-
-     public GameObject spawnObject;
-     private Vector3 spawnPoint;
-     public int minTimeSpawn = 2;
-     public int maxTimeSpawn = 10;
-     float timer = 0;
-     private float spawnInterval;
-     private float spawnTime;
+    public GameObject spawnObject;
+    private Vector3 spawnPoint;
+    public int minTimeSpawn = 2;
+    public int maxTimeSpawn = 10;
+    float timer = 0;
+    private float spawnInterval;
+    private float spawnTime;
+    private int spawnCount;
+    public int maxSpawn = 30;
  
-     void Start()
-     {
-         timer = 0;
-     }
+    void Start()
+    {
+        timer = 0;
+    }
  
-     private void Update()
-     {
-         timer += Time.deltaTime;
-         if(timer >= spawnTime)
-         {
-             spawnTime = Random.Range(minTimeSpawn, maxTimeSpawn);
-             Instantiate(spawnObject, spawnPoint, Quaternion.identity);
-             timer = 0;
-         }
-     }
+    private void Update()
+    {
+        if(spawnCount < maxSpawn){
+            timer += Time.deltaTime;
+            if(timer >= spawnTime)
+            {
+                spawnTime = Random.Range(minTimeSpawn, maxTimeSpawn);
+                Instantiate(spawnObject, spawnPoint, Quaternion.identity);
+                timer = 0;
+                spawnCount++;
+            }
+        }
+    }
 }
